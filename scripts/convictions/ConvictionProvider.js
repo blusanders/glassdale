c52a69fdc8e75f9a0712d399d3b3a5669b9f35e1
blusanders <blusanders@yahoo.com>
2021-01-27 07:40:04 -0600

    Officer select working

--- scripts/convictions/ConvictionProvider.js
+++ scripts/convictions/ConvictionProvider.js
@@ -14,3 +14,4 @@ export const getConvictions = () => {
         .then(convictionsArray => convictions = convictionsArray)  
 
 }
+
--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,9 @@
 console.log("Hello, I am the Main Module")
 import { CriminalList } from "./criminals/CriminalList.js";
 import { ConvictionSelect } from "./convictions/ConvictionSelect.js";
-import { OfficerList } from "./officers/OfficerList.js";
+import { OfficerSelect } from "./officers/OfficerSelect.js";
 
 CriminalList();
 ConvictionSelect();
-OfficerList();
+OfficerSelect();
 
